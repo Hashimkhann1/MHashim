89310c53458caaf635e08141f3f01ec94b6116e9
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-08-14 22:02:37 +0500

    updated

--- lib/res/my_colors/my_colors.dart
+++ lib/res/my_colors/my_colors.dart
@@ -7,5 +7,6 @@ class MyColors {
 
   static const Color blacColor = Color(0xFF000000);
   static const Color flutterColor = Color(0xFF045698);
+  static const Color whiteColor = Color(0xFFFFFFFF);
 
 }
