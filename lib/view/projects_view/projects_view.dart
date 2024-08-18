e1464272fe9f6c0cdde144e16b3e0468d73a946d
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-08-18 15:14:58 +0500

    animation is added

--- lib/view/projects_view/projects_view.dart
+++ lib/view/projects_view/projects_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:myportfolio/res/responsive/responsive.dart';
 
 class ProjectsView extends StatelessWidget {
@@ -6,32 +7,42 @@ class ProjectsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final width = MediaQuery.of(context).size.width;
-    final heigth = MediaQuery.of(context).size.height;
 
     return SizedBox(
-      width: 1250,
-      // height: heigth * 0.5,
+      width: Responsive.isMobile(context) ? width * 0.98 :  1250,
       child: GridView.builder(
+        // controller: _scrollController,
         itemCount: 6,
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: Responsive.isMobile(context) ? 1 : Responsive.isTablet(context) ? 2 : 3,
-            childAspectRatio: Responsive.isMobile(context) ? 0.35/0.22 : 0.35/0.3,
-            mainAxisSpacing: 5,
-            crossAxisSpacing: Responsive.isTablet(context) ? 0 : 5,
-          ),
-          itemBuilder: (context, index) {
-            return Container(
-              margin: EdgeInsets.symmetric(horizontal: Responsive.isDesktop(context) ? 0 : 5),
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(Responsive.isTablet(context) ? 10 : 5)
-              ),
-            );
-          }),
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: Responsive.isMobile(context)
+              ? 1
+              : Responsive.isTablet(context)
+              ? 2
+              : 3,
+          childAspectRatio: Responsive.isMobile(context) ? 0.35 / 0.22 : 0.35 / 0.3,
+          mainAxisSpacing: Responsive.isMobile(context) ? 8 : 5,
+          crossAxisSpacing: Responsive.isTablet(context) ? 0 : 5,
+        ),
+        itemBuilder: (context, index) {
+          return Container(
+            margin: EdgeInsets.symmetric(horizontal: Responsive.isDesktop(context) ? 0 : 5),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(Responsive.isTablet(context) ? 10 : 5),
+            ),
+          ).animate(
+            delay: (100 * index).ms, // Delay each item to create a staggered effect
+          ).scaleXY(
+            duration: Responsive.isMobile(context) ? index == 0 ? 1850.ms : index == 1 ? 2150.ms : (index * 740 ).ms : index == 0 ? 1450.ms : index == 1 ? 1650.ms : (index * 600 ).ms,
+            curve: Curves.easeOut
+            // curve: Curves.easeOut,
+          );
+        },
+      ),
     );
+
   }
 }
