ac060908d5cbbb42b298c57c56b822fe0caeade9
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-08-22 22:17:09 +0500

    updtaed

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "51c03c2858c667995ba3114d583a6e12",
+const RESOURCES = {"flutter_bootstrap.js": "da707a957ecd62893afbc07cc51e1060",
 "version.json": "7367c9ca1c69727a8bb09a7d2d21d48c",
 "index.html": "9a7c044ca16cc55ff6dc15b8ff2645ee",
 "/": "9a7c044ca16cc55ff6dc15b8ff2645ee",
-"main.dart.js": "dd501347b80c6d492991ac050d3817f4",
+"main.dart.js": "75d76fb860f3b0c31ac64a50d362f1c3",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
