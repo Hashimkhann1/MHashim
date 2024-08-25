6901d9d13f3c70f553a56a8725d538e49ced4865
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-08-25 17:21:12 +0500

    custome animation is added

--- lib/res/widgets/my_title_text.dart
+++ lib/res/widgets/my_title_text.dart
@@ -1,60 +1,109 @@
 
 
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:myportfolio/res/my_colors/my_colors.dart';
 import 'package:myportfolio/res/widgets/my_text.dart';
 import 'package:shimmer/shimmer.dart';
 
-class MyTitleText extends StatelessWidget {
+class MyTitleText extends StatefulWidget {
 
   final String? title;
   final double? fontSize;
   final bool? isAnimate;
-  const MyTitleText({super.key,required this.title , this.fontSize,this.isAnimate});
+  final double maxHeight;
+  final AnimationController controller;
+
+  const MyTitleText({super.key,required this.title , this.fontSize,this.isAnimate, required this.maxHeight,required this.controller});
+
+  @override
+  State<MyTitleText> createState() => _MyTitleTextState();
+}
+
+class _MyTitleTextState extends State<MyTitleText> with TickerProviderStateMixin {
+
+  late AnimationController controller;
+  late Animation<double> headingBackAnimation;
+  late Animation<double> textRevealAnimation;
+  late Animation<double> textOpicatyAnimation;
+
+  @override
+  void initState() {
+
+    controller = widget.controller;
+
+    headingBackAnimation = Tween<double>(begin: 0.0 ,end: 1.0).animate(CurvedAnimation(parent: controller, curve: Interval(0.3, 0.5 , curve: Curves.easeOut),));
+
+    textRevealAnimation  = Tween<double>(begin: 100.0, end: 0.0).animate(CurvedAnimation(parent: controller, curve: Interval(0.0, 0.3 , curve: Curves.easeOut)));
+
+    textOpicatyAnimation  = Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(parent: controller, curve: Interval(0.0, 0.3 , curve: Curves.easeOut)));
+
+
+    Future.delayed(Duration(milliseconds: 1000),() {
+      controller.forward();
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
 
     final height = MediaQuery.of(context).size.height;
 
-    return Stack(
-      alignment: Alignment.topCenter,
-      children: [
-        Positioned(
-          top: height * 0.009,
-          child: Container(
-            // width: width * 0.13,
-            height: 22,
-            decoration: BoxDecoration(
-                color: Colors.greenAccent.withOpacity(0.8)),
-            child: MyText(
-              title: title!,
-              fontSize: fontSize,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'Oswald',
-              letterSpacing: 3,
-              color: Colors.transparent,
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context , child) {
+        return Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            Positioned(
+              top: height * 0.009,
+              child: SizeTransition(
+                sizeFactor: headingBackAnimation,
+                axis: Axis.horizontal,
+                child: Container(
+                  // width: width * 0.13,
+                  height: 22,
+                  decoration: BoxDecoration(
+                      color: Colors.greenAccent.withOpacity(0.8)),
+                  child: MyText(
+                    title: widget.title!,
+                    fontSize: widget.fontSize,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Oswald',
+                    letterSpacing: 3,
+                    color: Colors.transparent,
+                  ),
+                ),
+              )
             ),
-          ).animate().fade(duration: 200.ms).scaleX(duration: const Duration(milliseconds: 2300)),
-        ),
-        Container(
-          child: Shimmer.fromColors(
-            baseColor: Colors.white,
-            highlightColor: MyColors.flutterColor,
-            period: Duration(milliseconds: 2000),
-            loop: 2,
-            child: MyText(
-              title: title!,
-              fontSize: fontSize,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'Oswald',
-              letterSpacing: 3,
-              color: const Color(0xFFFFFFFF),
+            LimitedBox(
+              maxHeight: widget.maxHeight,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.07,
+                // color: Colors.white,
+                padding: EdgeInsets.only(top: textRevealAnimation.value),
+                child: Shimmer.fromColors(
+                  baseColor: Colors.white,
+                  highlightColor: MyColors.flutterColor,
+                  period: Duration(milliseconds: 2000),
+                  loop: 2,
+                  child: FadeTransition(
+                    opacity: textOpicatyAnimation,
+                    child: MyText(
+                      title: widget.title!,
+                      fontSize: widget.fontSize,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Oswald',
+                      letterSpacing: 3,
+                      color: const Color(0xFFFFFFFF),
+                    ),
+                  ),
+                )
+              ),
             ),
-          )
-        ),
-      ],
+          ],
+        );
+      }
     );
   }
 }
